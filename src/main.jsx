d81b559d32b0e020e1b5ed5bfd995c6b545b8b07
Mikita Stankiewicz <itsmikita@gmail.com>
2022-10-25 07:25:05 +0200

    First working example (although very buggy)

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot( document.querySelector( "#root" ) ).render( <App /> );
+createRoot( document.querySelector( "#app" ) ).render( <App /> );
